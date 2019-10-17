46f2cbb72377ba6ae00fa6b26fbd8a0a21bd1047
Karen Olive <43334444+Karenolivia13@users.noreply.github.com>
2019-10-17 18:39:56 +0300

    Netflix commit

---
+++ netflix.js
@@ -0,0 +1,9 @@
+let movies  = document.getElementsByClassName('movies') ;
+let paragraphs = document.getElementsByClassName('paragraphs');
+movies.addEventListener('click', function(){
+   paragraphs.style.display = 'block';
+   paragraphs.style.backgroundColor ='rgba(37, 37, 37, 0.4)';
+});
+
+movies.addEventListener('')
+
